7ed3f8f3124aa245c1ea4f4c9f80478fce986f87
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2024-06-10 23:18:04 +0100

    Commit of unicafe steps 2 to 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,21 +4,52 @@ const Button = ({ onClick, text }) => {
   return <button onClick={onClick}>{text}</button>;
 };
 
+const Statistics = ({ good, neutral, bad, all }) => {
+  if (good || neutral || bad || all) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {(good - bad) / all} </p>
+        <p> positive {(good / all) * 100}%</p>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <p>No feedback given</p>
+    </div>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
 
   const handleGood = () => {
-    return setGood(good + 1);
+    const newGood = good + 1;
+    setGood(newGood);
+    console.log("good", newGood);
+    setAll(newGood + bad + neutral);
   };
 
   const handleNeutral = () => {
-    return setNeutral(neutral + 1);
+    const newNeutral = neutral + 1;
+    setNeutral(newNeutral);
+    console.log("neutral", newNeutral);
+    setAll(newNeutral + good + bad);
   };
 
   const handleBad = () => {
-    return setBad(bad + 1);
+    const newBad = bad + 1;
+    setBad(newBad);
+    console.log("bad", newBad);
+    setAll(newBad + good + neutral);
   };
 
   return (
@@ -27,10 +58,7 @@ const App = () => {
       <Button onClick={handleGood} text={"good"} />
       <Button onClick={handleNeutral} text={"neutral"} />
       <Button onClick={handleBad} text={"bad"} />
-      <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <Statistics good={good} bad={bad} neutral={neutral} all={all} />
     </div>
   );
 };
