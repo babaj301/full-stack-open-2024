c817abac44d60d325da3ffd42a358ab313401f5e
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2024-06-13 08:35:40 +0100

    Commit of phonebook exercise step 1

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const newObject = { name: newName };
+    setPersons(persons.concat(newObject));
+    setNewName;
+  };
+
+  const onChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const DisplayForm = ({ persons }) => {
+    return (
+      <div>
+        {persons.map((person) => {
+          return <p key={person.name}>{person.name}</p>;
+        })}
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={onSubmit}>
+        <div>
+          name: <input value={newName} onChange={onChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <DisplayForm key={persons.name} persons={persons} />
+    </div>
+  );
+};
+
+export default App;
