ffc0aabf1d9f0e29bafab275b114bb675df4ee97
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2024-06-14 14:55:16 +0100

    Commit for phonebook filter @testimoney

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: `040-1234567` },
+    { name: "Arto", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [input, setInput] = useState("Arto");
 
   const onSubmit = (event) => {
     event.preventDefault();
@@ -24,6 +28,12 @@ const App = () => {
     }
   };
 
+  const filtered = persons.filter((person) => {
+    return person.name.toLowerCase().includes(input.toLowerCase());
+  });
+
+  console.log(filtered);
+
   const handleName = (event) => {
     setNewName(event.target.value);
   };
@@ -31,10 +41,14 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
-  const DisplayForm = ({ persons }) => {
+  const handleInput = (event) => {
+    setInput(event.target.value);
+  };
+
+  const DisplayForm = ({ persons, filtered }) => {
     return (
       <div>
-        {persons.map((person) => {
+        {filtered.map((person) => {
           return (
             <p key={person.name}>
               {person.name} {person.number}
@@ -48,6 +62,9 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={input} onChange={handleInput} />
+      </div>
       <form onSubmit={onSubmit}>
         <div>
           name: <input value={newName} onChange={handleName} />
@@ -60,7 +77,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <DisplayForm key={persons.name} persons={persons} />
+      <DisplayForm key={persons.name} persons={persons} filtered={filtered} />
     </div>
   );
 };
